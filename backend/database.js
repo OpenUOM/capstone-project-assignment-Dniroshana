9f9b8a4eb0a070d1547d9e3b9cc510a8e9aa1515
Dasun Niroshana <dasunniroshana8@gmail.com>
2026-03-19 12:14:26 +0530

    Fix readTeacherInfo function to execute a proper SQL query instead of using dummy data

--- backend/database.js
+++ backend/database.js
@@ -37,7 +37,7 @@ const readTeachers = async () => {
 }
 
 const readTeacherInfo = async (id) => {
-    const sql = `dummy_data`
+    const sql = `SELECT * FROM teacher WHERE id = ?`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql, [id])
@@ -161,6 +161,7 @@ const deleteStudent = async (id) => {
             });
     });
 }
+
 
 module.exports = {
     readTeachers,
